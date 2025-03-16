67bb72d73c0a73688b470263041ce13dca76aa09
nileshwar-ganesh <nileshwar.gk@gmail.com>
2025-03-16 08:50:52 +0100

    update: day 004 mergesort working code (minor fixes)

--- code/004.py
+++ code/004.py
@@ -1,8 +1,6 @@
 def merge_sort(numbers: list[int], reverse=False) -> list[int]:
     """
-    Sorts a list of numbers using insertion sort algorithm
-    :param right:
-    :param left:
+    Sorts a list of numbers using merge sort algorithm
     :param numbers: the input list of numbers in random order
     :param reverse: determines ascending or descending. ascending, by default.
     :return: the sorted list, as per the requirement
@@ -13,7 +11,14 @@ def merge_sort(numbers: list[int], reverse=False) -> list[int]:
     sort_procedure(numbers, left, right, sign)
     return numbers
 
-def sort_procedure(numbers: list[int], left: int, right: int, sign: int) -> list[int]:
+def sort_procedure(numbers: list[int], left: int, right: int, sign: int):
+    """
+    :param numbers: the list of numbers, that need to be sorted
+    :param left: the left most index (of main and sub-lists, that keeps changing based on mid-point)
+    :param right: the right most index (of main and sub-lists, that keeps changing based on mid-point)
+    :param sign: determines the sort order, ascending or descending
+    :return: returns nothing as it modifies the position directly in original list
+    """
     if left < right:
         mid = (left + right) // 2
         # splitting into groups
